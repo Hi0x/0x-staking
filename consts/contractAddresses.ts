90f4e8ff6cbba169fc79ee4df3226a83b8439eb8
Hi0x <hi@hi0x.net>
2024-01-04 20:04:52 +0200

    Add files via upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x7D853F9A29b3c317773A461ed87F54cdDa44B0e0";
+export const tokenContractAddress =
+  "0x76174204db0bC7c3C817ce5b16C6Ef3900434B02";
+export const stakingContractAddress =
+  "0xC66fBE667C7832da71Bc375409940D6891De8F9d";
---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
+import { Navbar } from "../components/Navbar/Navbar";
+import "../styles/globals.css";
+
+// This is the chain your dApp will work on.
+const activeChain = "polygon";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+      activeChain={activeChain}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+    >
+      <Component {...pageProps} />
+      
+      <Navbar />
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+    
+  );
+  
+}
+
+export default MyApp;
